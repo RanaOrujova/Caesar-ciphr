ad09be1265eda5ed5a789c2df436e55bf0aa2649
Rana ♥ <renabaki2009@gmail.com>
2025-12-14 23:15:39 +0400

    Add files via upload

---
+++ caesar encoding/caesar cipher.py
@@ -0,0 +1,18 @@
+#encoding...
+
+# word=input()
+# k=int(input())
+# word1=""
+# for i in range(len(word)):
+#     word1+=chr(ord(word[i])+k)
+
+# print(word1)
+
+#decoding...
+word=input()
+k=int(input())
+word1=""
+for i in range(len(word)):
+    word1+=chr(ord(word[i])-k)
+
+print(word1)
